fad2957f70d619da02a801007f16a62da8de8b79
Muravei <kirillgorbachev16@yandex.ru>
2026-03-08 21:35:40 +0300

    0.0.5

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
 from data_coder import timing_coder
-from neuron import Neuron
+from neuron import Node
 import numpy as np
 import matplotlib.pyplot as plt
 
-image_path = [fr"D:\GitHub\Neural-network\src\datasets\unzip_datasets\mnist\images\image_{i}.jpg" for i in range(20)]
+image_path = [fr"D:\GitHub\Neural-network\src\datasets\unzip_datasets\mnist\images\image_{i}.jpg" for i in range(1)]
 
 allTime = 256 #ms
 step_time = 1 #ms
 
-timeLines = timing_coder(image_path,allTime,power=5)
+timeLines = timing_coder(image_path,allTime,power=1)
 
-l1 = np.array([Neuron(10) for _ in range(784)])
-l2 = np.array([Neuron() for _ in range(10)])
+node1 = Node(784,10)
 
-
-def plot_all_weights(l1, epoch, save_path='.'):
-    """Сохраняет одно изображение с картами весов для всех 10 выходных нейронов."""
+def plot_all_weights(W, epoch, save_path='.'):
     fig, axes = plt.subplots(2, 5, figsize=(15, 6))
     for i, ax in enumerate(axes.flat):
-        # Собираем веса от всех 784 нейронов первого слоя к i-му выходному нейрону
-        w_to_neuron = np.array([n.W[i] for n in l1]).reshape(28, 28)
+        w_to_neuron = np.array(1-W[:,i]).reshape(28, 28)
         im = ax.imshow(w_to_neuron, cmap='Greys', interpolation='nearest')
         ax.set_title(i)
         ax.axis('off')
@@ -28,77 +24,12 @@ def plot_all_weights(l1, epoch, save_path='.'):
     plt.savefig(f'{save_path}/weights_epoch_{epoch:03d}.png')
 
 for epoch in range(200):
-    for i in range(timeLines.shape[0]):
-        print(i)
-        timeline = timeLines[i]
-        for n in l1:
-            n.V = 0
-            n.trace = 0
-        for n in l2:
-            n.V = 0
-            n.trace = 0
-            n.threshold = 1.0
-
-        winner_found = False
-        winner_index = -1
-        for k in range(allTime):
-            tick = np.array(timeline[k])
-            for j, n in enumerate(l1):
-                n.LIF(tick[j])
-            for j, n in enumerate(l2):
-                n.LIF(sum([x.out[j] for x in l1]))
-            if not winner_found:
-                spiked = [i for i, n in enumerate(l2) if n.spike]
-                if len(spiked) > 0:
-                                # Выбираем первого спайкнувшего (можно случайного, если их несколько на одном шаге)
-                    winner_index = np.random.choice(spiked)
-                    winner_found = True
-                                # На текущем шаге подавляем остальных
-                    for i in spiked:
-                        if i != winner_index:
-                            l2[i].spike = 0
-                            l2[i].V = 0
-            else:
-                            # Победитель уже есть, подавляем всех остальных
-                for i, n in enumerate(l2):
-                    if i != winner_index:
-                        n.spike = 0
-                        n.V = 0
-            for n in l1:
-                n.STDP(l2)
-
-    plot_all_weights(l1, epoch)
-
-    fig, axes = plt.subplots(nrows=3,ncols=4,figsize=(12,12))
-
-    axes[0,0].plot(l2[0].v_list)
-    axes[0,0].set_title("V0")
-
-    axes[0,1].plot(l2[1].v_list)
-    axes[0,1].set_title("V1")
-
-    axes[0,2].plot(l2[2].v_list)
-    axes[0,2].set_title("V2")
-
-    axes[0,3].plot(l2[3].v_list)
-    axes[0,3].set_title("V3")
-
-    axes[1,0].plot(l2[4].v_list)
-    axes[1,0].set_title("V4")
-
-    axes[1,1].plot(l2[5].v_list)
-    axes[1,1].set_title("V5")
-
-    axes[1,2].plot(l2[6].v_list)
-    axes[1,2].set_title("V6")
-
-    axes[1,3].plot(l2[7].v_list)
-    axes[1,3].set_title("V7")
-
-    axes[2,0].plot(l2[8].v_list)
-    axes[2,0].set_title("V8")
-
-    axes[2,1].plot(l2[9].v_list)
-    axes[2,1].set_title("V9")
-
-    plt.savefig('V_plot.png')
+    print(epoch)
+    k = [0,0,0,0,0,0,0,0,0,0]
+    for timeline in timeLines:
+        for _ in range(20):
+            node1.step(timeline)
+            k += node1.layer_post.Spike
+    print(k)
+    plot_all_weights(node1.W, epoch)
+#plot_all_weights(node1.W, 1)
--- neuron.py
+++ neuron.py
@@ -1,36 +1,62 @@
 import numpy as np
 
-class Neuron():
-    def __init__(self,n_out=0):
-        self.V = 0
-        self.spike = 0
-        self.trace = 0
-        self.out = np.zeros(n_out)
-        self.W = np.random.random(size=n_out)
-        self.threshold = 1.0  # начальный порог
-        self.theta_increment = 0.05  # на сколько растёт после спайка
-        self.theta_decay = 0.001  # скорость возврата
-        self.v_list = []
+class Node():
+    def __init__(self,n_in,n_out):
+        self.layer_pre = Layer(n_in)
+        self.layer_post = Layer(n_out)
+        self.W = np.random.normal(0.5,0.3,(n_in,n_out))
+
+    def step(self,timeline):
+        self.layer_pre.V[:] = 0
+        self.layer_pre.Trace[:] = 0
+        self.layer_post.V[:] = 0
+        self.layer_post.Trace[:] = 0
+        winner_found = False
+        winner = -1
+        for I in timeline:
+            self.layer_pre.LIF(I)
+            self.layer_post.LIF(self.layer_pre.Spike @ self.W)
+
+            if not winner_found:
+                spiked = np.where(self.layer_post.Spike)[0]
+                if len(spiked) > 0:
+                    winner = np.random.choice(spiked)
+                    winner_found = True
+                    for i in spiked:
+                        if i != winner:
+                            self.layer_post.Spike[i] = 0.0
+                            self.layer_post.V[i] = 0.0
+            else:
+                mask = np.ones(len(self.layer_post.V), dtype=bool)
+                mask[winner] = False
+                self.layer_post.Spike[mask] = 0.0
+                self.layer_post.V[mask] = 0.0
+            self.STDP()
+
+    def STDP(self):
+        self.lr = 0.1
+        if np.any(self.layer_pre.Spike):
+            idx_pre = np.where(self.layer_pre.Spike)[0]
+            self.W[idx_pre, :] -= self.lr * self.W[idx_pre, :] * self.layer_post.Trace
+        if np.any(self.layer_post.Spike):
+            idx_post = np.where(self.layer_post.Spike)[0]
+            self.W[:, idx_post] += self.lr * (1 - self.W[:, idx_post]) * self.layer_pre.Trace[:, np.newaxis]     
+
+class Layer():
+    def __init__(self,n,Tresholds=False):
+        self.V = np.zeros(n)
+        self.Trace = np.zeros(n)
+        self.Spike = np.zeros(n)
+        self.Thresholds = np.ones(n) if Tresholds else None
 
     def LIF(self,I):
-        self.spike = 0
-        self.trace = self.trace - self.trace/100
-        self.V = self.V - self.V/100 + I
-        if self.V >= self.threshold:
-            self.spike = 1
-            self.trace = 1
-            self.V = 0
-            self.threshold += self.theta_increment  # повышаем порог после спайка
-            # медленно возвращаем порог к базовому значению
-        self.threshold = max(1.0, self.threshold - self.theta_decay)
-        self.out = self.spike * self.W
-        self.v_list.append(self.V)
-
-    def STDP(self,N_postList):
-        lr = 0.01
-        for i, n in enumerate(N_postList):
-            if self.spike:
-                self.q = self.W[i]
-                self.W[i] = self.W[i] - lr * self.W[i] * n.trace
-            if n.spike:
-                self.W[i] = self.W[i] + lr * (1 - self.W[i]) * self.trace
+        self.Spike = self.Spike * 0
+        self.Trace = self.Trace - self.Trace/20
+        self.V = self.V - self.V/20 + I
+        self.Spike = (self.V >= self.Thresholds).astype(float) if self.Thresholds is not None else (self.V >= 1.0).astype(float)
+        mask = self.Spike > 0
+        self.Trace[mask] = 1.0
+        self.V[mask] = 0
+        if self.Thresholds is not None:
+            self.Thresholds[mask] += 0.4
+            self.Thresholds = np.maximum(1,self.Thresholds - 0.1)
